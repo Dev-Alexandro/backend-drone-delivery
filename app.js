31922f02e6639a0e449c0de0ab6ccc6d7e574e6b
Alex Jordão <jordao.ti@gmail.com>
2023-09-11 20:14:21 -0300

    adjust port docker

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import deliveryRoutes from './src/routes/delivery.routes.js';
 import cors from 'cors';
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
